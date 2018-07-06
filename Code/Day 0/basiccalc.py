1e0e9ddcaf87073fb96e6f1b37353693274292da
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-07-06 15:40:58 +0530

    Added Day 0 Code

--- Code/Day 0/basiccalc.py
+++ Code/Day 0/basiccalc.py
@@ -1,1 +1,26 @@
-#Baisc Calculator program in python
+#Basic Calculator program in python
+
+num1 = int(input("enter the first number : "))
+num2 = int(input("enter the second number : "))
+choice=int(input("enter your choice 1 for sum,2 for subtract,3 for multiply and 4 for divide"))
+
+if choice==1:
+    add=(num1) + (num2)
+    print (add,"is the addition")
+
+elif choice==2:
+        sub=(num1)-(num2)
+        print(sub)
+
+elif choice==3:
+            mul=(num1)*(num2)
+            print(mul)
+
+elif choice==4:
+                div=(num1)/(num2)
+                print(div)
+
+else:
+                print("enter correct choice")
+
+
---
+++ Code/Day 0/table.py
@@ -0,0 +1,5 @@
+#print table 1 to 10 column 
+
+for i in range(1,11):
+    for j in range (1,11):
+        print(i,'x',j,'=',i*j,'\n')
