1944a4fe2a6d25c9e44daf1fb59e0054f430a13a
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-07-30 23:08:52 +0530

    Add Day 21

---
+++ Code/Day 21/sphere.py
@@ -0,0 +1,6 @@
+PI=3.14
+radius=float(input('please enter the radius of sphere'))
+sa=4*PI*radius*radius
+Volume=(4/3)*PI*radius*radius*radius
+print("\n The surface area of a sphere = %.2f"%sa)
+print("\n The Volume area of a sphere = %.2f"%Volume)
