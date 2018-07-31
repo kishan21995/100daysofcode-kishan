7522e1dba774ee201a8ee916e3536df86e1a57a5
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-07-31 18:34:51 +0530

    Add Day 22

---
+++ Code/Day 22/pattern6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+		char i,j;
+	for(i='A';i<='E';i++)
+{
+for(j='A';j<='E';j++)
+{
+printf("%c",j);
+}
+printf("\n");
+}	
+	return 0;
+}
---
+++ Code/Day 22/pattern7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void) {
+	// your code goes here
+		int i,j;
+	for(i=5;i>=1;i--)
+{
+for(j=5;j>=1;j--)
+{
+printf("%d",i);
+}
+printf("\n");
+}	
+	return 0;
+}
+ 
