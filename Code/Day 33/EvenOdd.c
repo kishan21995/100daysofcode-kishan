ac56c2cec49751d0e140a4bf0929d043378960bf
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-08-09 12:03:15 +0530

    Add Day 33

---
+++ Code/Day 33/EvenOdd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	printf("enter num \n");
+	scanf("%d ",&n);
+	if(n%2==0)
+	printf("num is even \n");
+	else
+	printf("num is odd \n");
+	return 0;
+}
