f7a1907a51c6937819b994df8e0e08755de28da5
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-08-10 23:05:42 +0530

    Add Day 38

---
+++ Code/Day 38/swap2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter two number");
+scanf("%d%d",&i&j);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping %d%d",a,b);
+    return 0;
+}
