e0e5d91416960d496e98087be27201caeb246b05
kishan gupta <38098955+mooteee@users.noreply.github.com>
2018-08-14 23:43:46 +0530

    Add Day 42

---
+++ Code/Day 42/spynum.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+main()
+{
+ 
+ int n,div,digit,s=0,p=1;
+ printf("Enter a number: ");
+ scanf("%d",&n);
+ for(div=n;div>0;div=div/10)
+ {
+  digit = div%10;
+  s += digit;
+  p *= digit;
+ }
+ 
+ if(s == p)
+ {
+  printf("It is spy number");
+ }
+ else
+ {
+  printf("Not spy number");
+ }
+ return 0;
+}#include<stdio.h>
+
+main()
+{
+ 
+ int n,div,digit,s=0,p=1;
+ printf("Enter a number: ");
+ scanf("%d",&n);
+ for(div=n;div>0;div=div/10)
+ {
+  digit = div%10;
+  s += digit;
+  p *= digit;
+ }
+ 
+ if(s == p)
+ {
+  printf("It is spy number");
+ }
+ else
+ {
+  printf("Not spy number");
+ }
+ return 0;
+}
